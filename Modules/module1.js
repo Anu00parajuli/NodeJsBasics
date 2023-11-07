b4190910b37bc7585c552b29262bea1e0aa2206b
Anu00parajuli <parajulianushka1213@gmail.com>
2023-11-07 23:44:11 +0545

    modules exported

---
+++ Modules/module1.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    console.log('Function was called');
+}
+
+var myString = 'String was called';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ Modules/module2.js
@@ -0,0 +1,7 @@
+module.exports={
+    myFunction : function(){
+        
+        console.log('Function from module 2 was called');
+    },
+    myString: 'String from module2 was called'
+}
